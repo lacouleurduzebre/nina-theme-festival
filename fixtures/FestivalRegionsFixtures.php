3822dd0e60695a4e190e5f4cc61c0a0b4d121c2a
Nadège Thierry <nadege@lacouleurduzebre.com>
2019-08-05 12:47:19 +0200

    fixture et css accueil

---
+++ fixtures/FestivalRegionsFixtures.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nadege
+ * Date: 2019-08-05
+ * Time: 09:30
+ */
+
+namespace Theme\festival\fixtures;
+
+
+use App\Entity\Langue;
+use App\Entity\Page;
+use App\Entity\SEO;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class FestivalRegionsFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+    }
+}
